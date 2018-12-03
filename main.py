77d017db5cd10cbbea8af2061db8a5ad9f2e4ea2
bindumadhavi.kura <bindumadhavi.kura@ggktech.com>
2018-12-03 16:13:46 +0530

    stable code

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import check_status
 import time
 import download_reports
 # Calling create scan function to create scans
-print "calling create_scan function" 
-create_scan.create_scan()
+#print "calling create_scan function" 
+#create_scan.create_scan()
 
 # Waiting for Ten mins to complete the scan
-time.sleep(300)
+#time.sleep(300)
 
 # Calling the Check status function
-print "calling scan status check function"
-check_status.check_status()
+#print "calling scan status check function"
+#check_status.check_status()
 
 #Calling the download_reports function to download the reports for the successful scans
 download_reports.download_reports()
